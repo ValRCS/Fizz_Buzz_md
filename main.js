fd67b1e58b70fa7356a05e02b86b1f8498e0ccbe
Valdis Saulespurens <valdis.s.coding@gmail.com>
2019-03-01 13:01:54 +0200

    add basic files

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = main;
+
+// You can add more functions here 
+function main() {
+    alert('hello started');
+    //Here you add your divs maybe call them from another function for clarity
+
+}
