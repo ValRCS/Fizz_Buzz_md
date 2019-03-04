09ad24eee9f208d8e4a2e0868848f44c27d5ab12
Valdis Saulespurens <valdis.s.coding@gmail.com>
2019-03-04 10:33:49 +0200

    Added color handling in events

--- main.js
+++ main.js
@@ -2,7 +2,32 @@ window.onload = main;
 
 // You can add more functions here 
 function main() {
-    alert('hello started');
-    //Here you add your divs maybe call them from another function for clarity
+    console.log("Started Main");
+    let mybut = document.getElementById('but1');
+    // mybut.onclick = myAlert;
+    mybut.addEventListener('click', myAlert);
+    mybut.addEventListener('click', myAlert2);
 
+    const mydiv = document.getElementById('mydiv');
+    mydiv.style.backgroundColor = 'grey';
+    //alert('hello started');
+    //Here you add your divs maybe call them from another function for clarity
+    const childdivs = mydiv.getElementsByTagName('div');
+    for (let i = 0; i < childdivs.length; i++) {
+        if (i % 2) {
+            // childdivs[i].style.backgroundColor = 'red';
+            childdivs[i].className += 'myred';
+        } else {
+            childdivs[i].className += 'mygreen';
+        }
+
+    }
+}
+
+function myAlert() {
+    // alert("Called my alert function");
+    console.log("Called myAlert ONE");
+}
+function myAlert2() {
+    console.log("Called myAlert2");
 }
