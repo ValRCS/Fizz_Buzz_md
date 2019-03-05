667781da0e55eaaa847b69a7cb3fe7a90bf4c5e3
Valdis Saulespurens <valdis.s.coding@gmail.com>
2019-03-05 11:33:28 +0200

    Process multiple values

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
 window.onload = main;
 
-
-
 const appState = {
 
     contentId: "contentbox",
-    begId: "begVal",
+    begId: "startVal",
     endId: "endVal",
     fizzId: "fizzVal",
     buzzId: "buzzId",
     submitId: "submitArgs",
     clearId: "clearContent",
+    ogContent: "<h2>Original Content</h2>",
+    rootEl: {},
+    args: {
+        beg: 1,
+        end: 100,
+        fizz: 3,
+        buzz: 5
+    },
     handleSubmit: function (ev) {
         console.log("Handling Submit");
         // console.log("Old endVal is" + appState.endVal);
-        console.log("Current endVal is: " + document.getElementById(appState.endId).value);
+        appState.args.beg = parseInt(document.getElementById(appState.begId).value);
+        appState.args.end = parseInt(document.getElementById(appState.endId).value);
+
+        for (let i = appState.args.beg; i < appState.args.end; i++) {
+            //TODO create function for creating elementwith arguments(tag, className, content, parent)
+            const newDiv = document.createElement('div');
+            newDiv.className = "myred";
+            newDiv.innerText = "Content is " + i;
+            appState.rootEl.appendChild(newDiv);
+        }
+
         //TODO create new element (div or p) with innerText of  document.getElementById(appState.endId).value
     },
     handleClear: (ev) => {
         console.log("Handling Clear");
         // console.log("Old endVal is" + appState.endVal);
         //TODO remove ALL elements under contentbox
+        document.getElementById(appState.contentId).innerHTML = appState.ogContent;
+
+    },
+    initializeState: function () {
+        console.log("Initialize State");
+        this.rootEl = document.getElementById(this.contentId);
+        document.getElementById(this.submitId).onclick = this.handleSubmit;
+        document.getElementById(this.clearId).onclick = this.handleClear;
     }
 }
 
@@ -29,8 +53,8 @@ const appState = {
 // You can add more functions here 
 function main() {
     console.log("Starting main");
-    document.getElementById(appState.submitId).onclick = appState.handleSubmit;
-    document.getElementById(appState.clearId).onclick = appState.handleClear;
+    appState.initializeState();
+
 
 }
 
